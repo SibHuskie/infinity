ea231fe26254dc372f253bd8b86f6053dd5f3209
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-21 19:28:36 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,4 +16,46 @@ async def on_ready():
     print(client.user.id)
     print("---------------")
     await client.change_presence(game=discord.Game(name='on Limited Infinity™'))
-client.run(os.environ['BOT_TOKEN'])
+
+# serverinfo
+@client.command(pass_context=True)
+async def mc(ctx):
+    author = ctx.message.author
+    msg = discord.Embed(colour=0xe3e550, description= "")
+    msg.title = ":closed_book: Member Count :closed_book: "
+    msg.add_field(name="MEMBERS", value=(len(ctx.message.server.members)), inline=True)
+    await client.say(embed=msg)
+
+# }userinfo <user>
+@client.command(pass_context=True)
+async def userinfo(ctx, userName: discord.Member = None):
+    member_role = discord.utils.get(ctx.message.server.roles, name='Member')
+    staff_role = discord.utils.get(ctx.message.server.roles, name='Staff')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0xe3e550, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if member_role in author.roles or staff_role in author.roles:
+        if userName == None:
+            msg.title = ""
+            msg.add_field(name="       :warning: ", value="`i!userinfo <user>`")
+        else:
+            imageurl = userName.avatar_url
+            msg.title = ":closed_book: USER INFORMATION"
+            msg.set_thumbnail(url=imageurl)
+            msg.add_field(name="NAME:", value=(userName), inline=True)
+            msg.add_field(name="ID:", value=(userName.id), inline=True)
+            msg.add_field(name="CREATED AT:", value=(userName.created_at), inline=True)
+            msg.add_field(name="JOINED AT:", value=(userName.joined_at), inline=True)
+            msg.add_field(name="STATUS:", value=(userName.status), inline=True)
+            msg.add_field(name="IS BOT:", value=(userName.bot), inline=True)
+            msg.add_field(name="GAME:", value="Playing {}".format(userName.game), inline=True)
+            msg.add_field(name="NICKNAME:", value=(userName.nick), inline=True)
+            msg.add_field(name="TOP ROLE:", value=(userName.top_role), inline=True)
+            msg.add_field(name="VOICE CHANNEL:", value=(userName.voice_channel), inline=True)
+    await client.say(embed=msg)
+    print("============================================================")
+    print("<userinfo <user>")
+    print("{} ### {}".format(author, author.id))
+    print("============================================================")
+    client.run(os.environ['BOT_TOKEN'])
