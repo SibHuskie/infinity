9188a6d966f8c6fc0c19cb4a13a28ba1548b32f9
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-21 18:59:11 +1000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import logging
+import asyncio
+import random
+import time
+import os
+
+client = commands.Bot(command_prefix="i!")
+footer_text = "Limited Infinity™"
+
+@client.event
+async def on_ready():
+    print("Logged in as:")
+    print(client.user.name)
+    print(client.user.id)
+    print("---------------")
+    await client.change_presence(game=discord.Game(name='on Limited Infinity™'))
+client.run(os.environ['BOT_TOKEN'])
