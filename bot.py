764def0940120ae95481c10c56cf2d9bed688a0d
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-23 18:37:46 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import asyncio
 import random
 import time
 import os
+import safygiphy
 
 client = commands.Bot(command_prefix="i!")
 footer_text = "Limited Infinity™"
@@ -16,6 +17,53 @@ async def on_ready():
     print(client.user.id)
     print("---------------")
     await client.change_presence(game=discord.Game(name='on Limited Infinity™'))
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('i!test'):
+        await client.send_message(message.channel, "Test bestanden")
+
+    if message.content.lower().startswith('i!coin'): #Coinflip 50/50% chance kopf oder zahl
+        choice = random.randint(1,2)
+        if choice == 1:
+            await client.add_reaction(message, '🌑')
+        if choice == 2:
+            await client.add_reaction(message, '🌕')
+
+    if message.content.startswith('i!game') and message.author.id == DEIN_USERNAME:
+        game = message.content[6:]
+        await client.change_presence(game=discord.Game(name=game))
+        await client.send_message(message.channel, "Ich habe meinen Status zu {0} geaendert".format(game))
+
+    if message.content.startswith('?bild'):
+        response = requests.get("https://media3.giphy.com/media/6C9CMGMFtzzbO/giphy.gif", stream=True)
+        await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='bild.gif', content='Test Bild.')
+
+    if message.content.startswith('i!uptime'):
+        await client.send_message(message.channel, "`Ich bin schon {0} stunde/n und {1} minuten online auf {2}. `".format(hour, minutes, message.server))
+
+    if message.content.startswith('i!gif'):
+        gif_tag = message.content[5:]
+        rgif = g.random(tag=str(gif_tag))
+        response = requests.get(
+            str(rgif.get("data", {}).get('image_original_url')), stream=True
+        )
+        await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif')
+
+    if message.content.startswith('i!fun'):
+        gif_tag = "fun"
+        rgif = g.random(tag=str(gif_tag))
+        response = requests.get(
+            str(rgif.get("data", {}).get('image_original_url')), stream=True
+        )
+        await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif')
+
+
+@client.event
+async def on_member_join(member):
+    serverchannel = member.server.default_channel
+    msg = "Willkommen {0} auf {1}".format(member.mention, member.server.name)
+    await client.send_message(serverchannel, msg)
 
 # serverinfo
 @client.command(pass_context=True)
