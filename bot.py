506f1230760ddc4028787d19f613fc26d1108e89
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-26 10:09:30 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,17 +17,6 @@ async def on_ready():
     print("---------------")
     await client.change_presence(game=discord.Game(name='on Limited Infinity™'))
 
-# EVENT - JOIN / LEAVE
-@client.async_event
-async def on_member_join(member):
-    msg = "Welcome to {1}! 🎉 {0}. Make sure to read <#446594082731720705> and get some roles in <#448456540597256192> feel free to DM the owner for queries and enjoy your stay here 😉 ".format(member.mention, member.server.name)
-    channel = discord.utils.get(name="✧》main-chat", member.server.channels)
-    await client.send_message(channel, msg)
-    print("============================================================")
-    print("JOIN EVENT")
-    print("{} ### {}".format(userName, userName.id))
-    print("============================================================")
-
 # serverinfo
 @client.command(pass_context=True)
 async def mc(ctx):
